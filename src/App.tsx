d3588b59c2105c7cd5b51682a935bb6e6e19c0cb
uptu <bh1000@live.com>
2023-04-20 02:39:22 -0600

    Initial refactor: implemented dark mode and basic theming

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './assets/banner.webp';
+import './App.scss';
+import './index.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Under Construction
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
