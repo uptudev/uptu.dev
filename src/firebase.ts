f72edf7daafbe87ba7e0b76df18315faae4c0727
uptu <uptu@uptu.dev>
2023-05-21 03:20:48 -0600

    Minor changes, fixed database domain

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,15 +3,27 @@ import { getDatabase } from "firebase/database";
 import { getAuth, UserCredential } from "firebase/auth";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBpJSHLszGkpFHNvSCQ-1l7L8az09wNkc0",
-    authDomain: "vuetut-7992c.firebaseapp.com",
-    projectId: "vuetut-7992c",
-    storageBucket: "vuetut-7992c.appspot.com",
-    messagingSenderId: "1084894783495",
-    appId: "1:1084894783495:web:e00a702f6da2cbbf01a2a2", 
-    measurementId: "G-PYNKKC834H",
+
+  apiKey: "AIzaSyBpJSHLszGkpFHNvSCQ-1l7L8az09wNkc0",
+
+  authDomain: "vuetut-7992c.firebaseapp.com",
+
+  databaseURL: "https://vuetut-7992c-default-rtdb.firebaseio.com",
+
+  projectId: "vuetut-7992c",
+
+  storageBucket: "vuetut-7992c.appspot.com",
+
+  messagingSenderId: "1084894783495",
+
+  appId: "1:1084894783495:web:e00a702f6da2cbbf01a2a2",
+
+  measurementId: "G-PYNKKC834H"
+
 };
+
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
